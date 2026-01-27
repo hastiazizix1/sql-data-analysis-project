3436898179b87204dc67af316acf381f4814f047
Hasti Azizi <azizihasti12345@gmail.com>
2026-01-27 18:08:28 +0300

    Update change_over_time.sql

--- scripts/change_over_time.sql
+++ scripts/change_over_time.sql
@@ -1,17 +1,3 @@
-USE DataWarehouseAnalytics;
-GO
-
-SELECT 
-YEAR(order_date) AS order_year,
-MONTH(order_date) AS order_month,
-SUM(sales_amount) as total_sales,
-COUNT(DISTINCT customer_key) as total_customers,
-SUM(quantity) as total_quantity
-FROM gold.fact_sales
-WHERE order_date IS NOT NULL
-GROUP BY YEAR(order_date), MONTH(order_date)
-ORDER BY YEAR(order_date), MONTH(order_date)
-
 SELECT 
 DATETRUNC(month, order_date) AS order_date,
 SUM(sales_amount) as total_sales,
@@ -21,14 +7,3 @@ FROM gold.fact_sales
 WHERE order_date IS NOT NULL
 GROUP BY DATETRUNC(month, order_date)
 ORDER BY DATETRUNC(month, order_date)
-
-
-SELECT 
-FORMAT(order_date, 'yyyy-MMM') AS order_date,
-SUM(sales_amount) as total_sales,
-COUNT(DISTINCT customer_key) as total_customers,
-SUM(quantity) as total_quantity
-FROM gold.fact_sales
-WHERE order_date IS NOT NULL
-GROUP BY FORMAT(order_date, 'yyyy-MMM')
-ORDER BY FORMAT(order_date, 'yyyy-MMM')
